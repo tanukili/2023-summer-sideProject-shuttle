fb0e6c7f4502e219426430b359d18146b7dbb052
tanukile <backpacker320@gmail.com>
2023-08-27 22:04:14 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script>
+import { def } from '@vue/shared';
+import TheWelcome from '../components/TheWelcome.vue';
+
+export default {
+  comments: {
+    TheWelcome,
+    def
+  },
+};
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
