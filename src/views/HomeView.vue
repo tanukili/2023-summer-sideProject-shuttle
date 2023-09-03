290fd335009f5e4f38bb5eabbd961d26159f9264
tanukile <backpacker320@gmail.com>
2023-09-03 20:18:28 +0800

    feat: index banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,89 +1,52 @@
 <script>
-import HeaderNavbar from '../components/HeaderNavbar.vue';
-
-export default {
-  // data() {
-  //   return {
-  //     data: {},
-  //   };
-  // },
-  // mounted() {
-  //   this.$http.get('https://randomuser.me/api/').then((response) => {
-  //     console.log(response.data);
-  //   });
-  // },
-  comments: {
-    HeaderNavbar,
-  },
-};
 </script>
 
 <template>
-  <div>
-    <h1>首頁內容</h1>
-    <small>小字</small>
-    <button type="button" class="btn btn-secondary me-3">Primary</button>
-    <button type="button" class="btn btn-secondary me-3 disabled">
-      Primary
-    </button>
-    <!-- Button trigger modal -->
-    <button
-      type="button"
-      class="btn btn-primary"
-      data-bs-toggle="modal"
-      data-bs-target="#exampleModal"
-    >
-      modal
-    </button>
-
-    <!-- Modal -->
-    <div
-      class="modal fade"
-      id="exampleModal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">...</div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
+  <div class="bg-banner">
+    <div class="container">
+      <div class="banner-content">
+        <h1 class="fs-2 fw-bold text-white pb-3">
+          一封真摯的邀請函<br/>
+          <span class="py-3 d-inline-block">
+            給予手織
+            <span class="select-icon align-bottom">
+              <select
+                class="form-select fs-5 fw-bold d-inline-block w-auto text-primary py-2"
+                aria-label="Default select example"
+              >
+                <option selected>無經驗</option>
+                <option value="beginner">初學者</option>
+                <option value="amateur">愛好者</option>
+                <option value="artist">創作者</option>
+              </select>
+            </span>
+          </span>
+          的你
+        </h1>
+        <button class="btn btn-secondary w-100 fs-5 py-3 mt-3" type="button">
+          現在開啟
+          <span class="material-symbols-outlined align-top ms-1">east</span>
+        </button>
       </div>
     </div>
-    <div class="card" style="max-width: 400px">
-      <img
-        src="../assets/images/product01.jpg"
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-    <p>BS5 結束</p>
   </div>
 </template>
+
+<style>
+.bg-banner {
+  /* 加上半透明遮罩 */
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url(../assets/images/banner.jpg);
+  padding: 235px 0 240px 0;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.banner-content {
+  max-width: 432px;
+  margin-left: 120px;
+  @media (max-width: 768px) {
+    margin: 0 auto;
+  }
+}
+</style>
