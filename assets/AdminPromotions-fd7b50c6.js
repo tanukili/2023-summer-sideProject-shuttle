56f543c208ad24a897879b208be5e8ebd411c8f7
tanukile <backpacker320@gmail.com>
2023-12-10 16:03:51 +0800

    deploy

--- assets/AdminPromotions-fd7b50c6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,f as o,g as s,i as t}from"./index-815cd100.js";const c={},n={class:"about"},_=t("h1",null,"優惠活動列表",-1),a=[_];function r(i,d){return o(),s("div",n,a)}const l=e(c,[["render",r]]);export{l as default};
--- assets/BackgroundBanner-92ebfbd2.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,f as a,g as r,i as s,J as t}from"./index-815cd100.js";const o={props:["bannerImg"]};function c(_,l,e,d,i,p){return a(),r("div",null,[s("div",{class:"bg-banner banner-mask w-100 z-n1",style:t(e.bannerImg)},null,4)])}const m=n(o,[["render",c]]);export{m as B};
