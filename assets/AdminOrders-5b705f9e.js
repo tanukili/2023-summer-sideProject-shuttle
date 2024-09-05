d25d481435580964dbd96046a5f27af5802ad12f
tanukile <backpacker320@gmail.com>
2024-09-05 22:52:29 +0800

    Updates

--- assets/AdminOrders-5b705f9e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as o,a as t}from"./index-686eedae.js";const c={},n={class:"about"},_=t("h1",null,"後台訂單列表",-1),a=[_];function r(d,i){return s(),o("div",n,a)}const f=e(c,[["render",r]]);export{f as default};
--- assets/MemberFeedback-fcbb5e48.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as s,a as o}from"./index-686eedae.js";const t={},a={class:"bg-primary py-3"},r=o("h1",null,"課程回饋",-1),_=[r];function n(d,i){return c(),s("div",a,_)}const f=e(t,[["render",n]]);export{f as default};
